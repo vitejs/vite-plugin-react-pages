e0ab5dba1fb27898683945958e80a21bd773196e
csr632 <632882184@qq.com>
2020-06-11 01:39:14 +0800

    init doc site

---
+++ doc-site/pages/_render.tsx
@@ -0,0 +1,11 @@
+import { createRender } from 'vite-pages-theme-basic'
+
+export default createRender({
+  topNavs: [
+    {
+      text: 'Github ⭐',
+      href: 'https://github.com/csr632/vite-plugin-react-pages',
+    },
+  ],
+  logo: 'Vite Pages',
+})
