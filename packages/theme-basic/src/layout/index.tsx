020a78177f76e08772976b7efd1f4f6709608615
sirui.csr <sirui.csr@alibaba-inc.com>
2020-06-10 10:50:29 +0800

    implement theme-basic

---
+++ packages/theme-basic/src/layout/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Topbar from './top-bar'
+import type { ITopNavData } from './top-bar'
+import SideMenu from './side-menu'
+import type { ISideMenuData } from './side-menu'
+import s from './style.module.css'
+
+import './global.css'
+
+interface IProps {
+  Content: React.ComponentType
+  sideMenuData?: ISideMenuData[]
+  pages: any
+  topNavs?: ITopNavData[]
+  logo?: React.ReactNode
+}
+
+const Layout: React.FC<IProps> = ({
+  Content,
+  sideMenuData,
+  pages,
+  topNavs,
+  logo,
+}) => {
+  return (
+    <div className={s.layout}>
+      <Topbar topNavs={topNavs} logo={logo} />
+      <div className={s.body}>
+        <SideMenu pages={pages} data={sideMenuData} />
+        <div>
+          <Content />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Layout
