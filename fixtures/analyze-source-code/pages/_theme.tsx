db211975952c61e026ab5557eaf6edcf41cfb8bf
csr632 <632882184@qq.com>
2020-08-25 02:44:21 +0800

    fix analyze-source-code fixture

--- fixtures/analyze-source-code/pages/_theme.tsx
+++ fixtures/analyze-source-code/pages/_theme.tsx
@@ -1,23 +1,10 @@
 import React from 'react'
-import type { ICreateTheme } from 'vite-plugin-react-pages'
+import type { ITheme } from 'vite-plugin-react-pages'
 
-const theme: ICreateTheme = (pages) => {
-  return {
-    initialLoading() {
-      return <p>Loading</p>
-    },
-    loaded(pageData) {
-      const Component = pageData.default
-      return <Component />
-    },
-    loadError(error, pageStaticData) {
-      console.error('load error!', { error, pageStaticData, pages })
-      return <p>Load error, see console.error</p>
-    },
-    noPageMatch() {
-      return <p>Page Not Found.</p>
-    },
-  }
+const theme: ITheme = ({ loadedData, loadState }) => {
+  if (loadState.type !== 'loaded') return <p>Loading</p>
+  const Component = loadedData[loadState.routePath].main.default
+  return <Component />
 }
 
 export default theme
--- fixtures/analyze-source-code/pages/index$.tsx
+++ fixtures/analyze-source-code/pages/index$.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
 
 import s from './index.module.css'
@@ -8,25 +8,29 @@ import demoSource from '../demos/demo1?analyzeSource'
 interface IProps {}
 
 const IndexPage: React.FC<IProps> = (props) => {
+  useEffect(() => {
+    console.log('Source code analysis result', demoSource)
+  }, [])
+
   return (
     <div>
       <p>Load and render "demos/demo1":</p>
       <Demo />
-      <hr/>
-      <p>Here is source code for "demos/demo1":</p>
-      {Object.entries(demoSource.modules).map(([moduleName, module]: any) => {
-        return (
-          <div key={moduleName}>
-            <p className={s.moduleName}>{moduleName}</p>
-            <LiveProvider code={module.code}>
-              <LiveEditor />
-            </LiveProvider>
-          </div>
+      <p style={{ marginTop: 48 }}>Here is source code for "demos/demo1":</p>
+      {Object.entries(demoSource.modules)
+        .sort(([moduleNameA], [moduleNameB]) =>
+          moduleNameA.localeCompare(moduleNameB)
         )
-      })}
-      <hr/>
-      <p>Source code analysis data:</p>
-      <pre>{JSON.stringify(demoSource, null, 2)}</pre>
+        .map(([moduleName, module]: any) => {
+          return (
+            <div key={moduleName}>
+              <p className={s.moduleName}>{moduleName}</p>
+              <LiveProvider code={module.code}>
+                <LiveEditor />
+              </LiveProvider>
+            </div>
+          )
+        })}
     </div>
   )
 }
