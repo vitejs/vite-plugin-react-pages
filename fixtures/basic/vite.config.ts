319ab5a5d282a538dae1813cb1decfd0767f2cb3
Sirui Chen <632882184@qq.com>
2021-01-06 01:01:31 +0800

    fix basic fixture

--- fixtures/basic/vite.config.ts
+++ fixtures/basic/vite.config.ts
@@ -1,9 +1,13 @@
 import type { UserConfig } from 'vite'
-import pages from 'vite-plugin-react-pages-2'
 import * as path from 'path'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import mdx from 'vite-plugin-mdx'
+import pages from 'vite-plugin-react-pages-2'
 
 module.exports = {
   plugins: [
+    reactRefresh(),
+    mdx(),
     pages({
       pagesDir: path.join(__dirname, 'pages'),
     }),
