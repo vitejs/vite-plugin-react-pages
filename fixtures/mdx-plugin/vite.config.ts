6b9bc10cc49be22f75ce20244a58c8d6a7ae6158
Sirui Chen <632882184@qq.com>
2021-01-06 00:48:50 +0800

    fix mdx fixtures

--- fixtures/mdx-plugin/vite.config.ts
+++ fixtures/mdx-plugin/vite.config.ts
@@ -1,15 +1,16 @@
 import type { UserConfig } from 'vite'
 import mdx from 'vite-plugin-mdx'
-import react from 'vite-plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+
 import remarkToc from 'remark-toc'
 import remarkSlug from 'remark-slug'
 
 module.exports = {
   jsx: 'react',
   plugins: [
-    react,
+    reactRefresh(),
     mdx({
-      remarkPlugins: [remarkToc, remarkSlug]
-    })
-  ]
+      remarkPlugins: [remarkToc, remarkSlug],
+    }),
+  ],
 } as UserConfig
