f935277d6a9f3a557318dfc22e2d1b0d2c44d86d
Sirui Chen <632882184@qq.com>
2021-01-08 01:43:53 +0800

    fix create project

---
+++ packages/create-project/template-lib-monorepo/packages/library-demos/demos/Button/demo1.tsx
@@ -0,0 +1,13 @@
+/**
+ * @title Button Demo1 Title
+ * @description Button demo1 description
+ */
+
+import React from 'react'
+import { Button } from 'my-library'
+
+const Demo1: React.FC = () => {
+  return <Button>demo1</Button>
+}
+
+export default Demo1
---
+++ packages/create-project/template-lib-monorepo/packages/library-demos/pages/$.tsx
@@ -0,0 +1,18 @@
+/**
+ * @title Index Page Title
+ */
+
+import React from 'react'
+
+interface IProps {}
+
+const Page: React.FC<IProps> = (props) => {
+  return (
+    <div>
+      <h1>Index page</h1>
+      <p>You can put an introduction here.</p>
+    </div>
+  )
+}
+
+export default Page
