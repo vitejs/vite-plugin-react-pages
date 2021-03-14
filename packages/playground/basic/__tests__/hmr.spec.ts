3711e08a815ed8b6c4e1c63104305fdd44a384d4
csr632 <632882184@qq.com>
2021-03-14 22:34:45 +0800

    test: add hmr test

--- packages/playground/basic/__tests__/hmr.spec.ts
+++ packages/playground/basic/__tests__/hmr.spec.ts
@@ -1,3 +1,16 @@
+import { editFile, untilUpdated, isBuild, getColor } from '../../testUtils'
+
 test('should hmr', async () => {
-  // TODO
+  await untilUpdated(() => page.textContent('#root'), 'IndexPage')
+  editFile('pages/index$.tsx', (code) =>
+    code.replace(`<div>IndexPage</div>`, `<div>hmr works!</div>`)
+  )
+  await untilUpdated(() => page.textContent('#root'), 'hmr works!')
+
+  await page.goto(viteTestUrl + '/page1')
+  await untilUpdated(() => page.textContent('.page'), 'Page1')
+  const el = await page.$('.page')
+  await untilUpdated(() => getColor(el), 'blue')
+  editFile('pages/style.scss', (code) => code.replace(`color: blue`, `color: red`))
+  await untilUpdated(() => getColor(el), 'red')
 })
