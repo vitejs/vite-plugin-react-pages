b8377d810d976b45472f7bd8dedeb5c4802cba86
csr632 <632882184@qq.com>
2021-03-15 01:21:55 +0800

    test: add tests for PageStrategy

--- packages/playground/use-theme/__tests__/use-theme.spec.ts
+++ packages/playground/use-theme/__tests__/use-theme.spec.ts
@@ -1,5 +1,4 @@
-import { ElementHandle } from 'playwright-chromium'
-import { editFile, untilUpdated, isBuild, getColor } from '../../testUtils'
+import { editFile, untilUpdated, removeFile, getColor } from '../../testUtils'
 
 test('should render theme pages', async () => {
   await page.waitForLoadState('networkidle')
@@ -19,4 +18,33 @@ test('should render theme pages', async () => {
   ])
   await page.waitForSelector('text=React Box')
   expect(await getColor('text=React Box')).toBe('blue')
+})
+
+test('hmr', async () => {
+  await page.goto(viteTestUrl)
+  const sideNav = await page.$('.vp-theme-aside-navigation .vp-theme-menu')
+
+  expect(await sideNav.textContent()).toBe(
+    'index page titlepage1 titlepage2 titlepage3 titlepage4 title'
+  )
+  editFile('pages/page1$.tsx', (code) =>
+    code.replace('@title page1 title', '@title page1 updated title')
+  )
+  await untilUpdated(
+    () => sideNav.textContent(),
+    'index page titlepage1 updated titlepage2 titlepage3 titlepage4 title'
+  )
+  // put it before page1
+  editFile('pages/page2$.md', (code) => code.replace('sort: 2', 'sort: 0.5'))
+  await untilUpdated(
+    () => sideNav.textContent(),
+    'index page titlepage2 titlepage1 updated titlepage3 titlepage4 title'
+  )
+
+  // delete page2
+  await removeFile('pages/page2$.md')
+  await untilUpdated(
+    () => sideNav.textContent(),
+    'index page titlepage1 updated titlepage3 titlepage4 title'
+  )
 })
