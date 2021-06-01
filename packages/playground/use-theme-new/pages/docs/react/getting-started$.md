086c226b98c7277d0f1bdc1a091ef0dcb0fc7096
csr632 <632882184@qq.com>
2021-06-01 10:13:36 +0800

    add markdown style

--- packages/playground/use-theme-new/pages/docs/react/getting-started$.md
+++ packages/playground/use-theme-new/pages/docs/react/getting-started$.md
@@ -3,6 +3,8 @@ order: 2
 title: Getting Started
 ---
 
+# Getting Started
+
 Ant Design React is dedicated to providing a **good development experience** for programmers. Before starting, it is recommended to learn [React](https://reactjs.org) and [ES2015](http://babeljs.io/docs/learn-es2015/) first, and correctly install and configure [Node.js](https://nodejs.org/) v8 or above.
 
 The official guide also assumes that you have intermediate knowledge about HTML, CSS, and JavaScript, and React. If you are just starting to learn front-end or React, it may not be the best idea to use the UI framework as your first step.
--- packages/theme-basic-new/src/Layout/index.tsx
+++ packages/theme-basic-new/src/Layout/index.tsx
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react'
 import { Layout, ConfigProvider, Row, Col } from 'antd'
+import { useStaticData } from 'vite-plugin-react-pages/client'
+import 'github-markdown-css/github-markdown.css'
 
 const { Content } = Layout
 
@@ -14,11 +16,13 @@ ConfigProvider.config({
 
 interface Props {}
 
-const AppLayout: React.FC<Props> = (props) => {
+const AppLayout: React.FC<Props> = () => {
   const { sideNavs } = useContext(themeConfigCtx)
   const themeProps = useContext(themePropsCtx)
   const { loadState, loadedData } = themeProps
   const Main = loadedData[loadState.routePath]?.main?.default
+  const staticData = useStaticData(loadState.routePath)
+  const isMarkdown = staticData?.main?.sourceType === 'md'
 
   const sideNavsData =
     typeof sideNavs === 'function' ? sideNavs(themeProps) : sideNavs
@@ -34,7 +38,13 @@ const AppLayout: React.FC<Props> = (props) => {
             </Col>
           )}
           <Col flex="auto" style={{ minWidth: 0 }}>
-            <Content className={s.content}>{Main && <Main />}</Content>
+            <Content
+              className={[s.content, isMarkdown && 'markdown-body']
+                .filter(Boolean)
+                .join(' ')}
+            >
+              {Main && <Main />}
+            </Content>
           </Col>
         </Row>
       </div>
