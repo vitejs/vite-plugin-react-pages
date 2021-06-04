d2e31c9816ad7a1b99d9cbd0e5c9cc3b316c1e7e
csr632 <632882184@qq.com>
2021-06-05 01:04:49 +0800

    update fixture for new theme

--- packages/playground/use-theme-new/pages/_theme.tsx
+++ packages/playground/use-theme-new/pages/_theme.tsx
@@ -6,16 +6,16 @@ export default createTheme({
   logo: <div style={{ marginLeft: 40, fontWeight: 'bold' }}>Vite Pages</div>,
   topNavs: [
     {
-      label: 'Design',
-      path: '/docs/spec/introduce',
-    },
-    {
-      label: 'Develop',
-      path: '/docs/react/introduce',
-    },
-    {
       label: 'Components',
       path: '/components/overview',
+    },
+    {
+      label: 'Guide',
+      path: '/guide/introduce',
+    },
+    {
+      label: 'Reference',
+      path: '/reference/glossary',
     },
     {
       label: 'Github',
@@ -26,7 +26,7 @@ export default createTheme({
       children: [
         {
           label: 'Resources',
-          path: '/docs/resources',
+          path: '/resources',
         },
         {
           label: 'Vite',
@@ -46,107 +46,107 @@ export default createTheme({
       </Button>
     )
   },
-  sideNavs: ({ loadState }) => {
-    if (loadState.routePath.startsWith('/docs/spec')) {
-      return [
-        {
-          subMenu: 'Ant Design',
-          children: [
-            {
-              label: 'Introduction',
-              path: '/docs/spec/introduce',
-            },
-            {
-              label: 'Design Values',
-              path: '/docs/spec/values',
-            },
-          ],
-        },
-        {
-          subMenu: 'Design Patterns',
-          children: [
-            { label: 'Overview', path: '/docs/spec/overview' },
-            {
-              group: 'Principles',
-              children: [
-                {
-                  label: 'Proximity',
-                  path: '/docs/spec/proximity',
-                },
-                {
-                  label: 'Alignment',
-                  path: '/docs/spec/alignment',
-                },
-              ],
-            },
-          ],
-        },
-      ]
-    }
-    if (loadState.routePath.startsWith('/components/')) {
-      return [
-        { label: 'Components Overview', path: '/components/overview' },
+  // sideNavs: ({ loadState }) => {
+  //   if (loadState.routePath.startsWith('/docs/spec')) {
+  //     return [
+  //       {
+  //         subMenu: 'Ant Design',
+  //         children: [
+  //           {
+  //             label: 'Introduction',
+  //             path: '/docs/spec/introduce',
+  //           },
+  //           {
+  //             label: 'Design Values',
+  //             path: '/docs/spec/values',
+  //           },
+  //         ],
+  //       },
+  //       {
+  //         subMenu: 'Design Patterns',
+  //         children: [
+  //           { label: 'Overview', path: '/docs/spec/overview' },
+  //           {
+  //             group: 'Principles',
+  //             children: [
+  //               {
+  //                 label: 'Proximity',
+  //                 path: '/docs/spec/proximity',
+  //               },
+  //               {
+  //                 label: 'Alignment',
+  //                 path: '/docs/spec/alignment',
+  //               },
+  //             ],
+  //           },
+  //         ],
+  //       },
+  //     ]
+  //   }
+  //   if (loadState.routePath.startsWith('/components/')) {
+  //     return [
+  //       { label: 'Components Overview', path: '/components/overview' },
 
-        {
-          group: 'General',
-          children: [
-            {
-              label: 'Button',
-              path: '/components/button',
-            },
-            {
-              label: 'Icon',
-              path: '/components/icon',
-            },
-          ],
-        },
+  //       {
+  //         group: 'General',
+  //         children: [
+  //           {
+  //             label: 'Button',
+  //             path: '/components/button',
+  //           },
+  //           {
+  //             label: 'Icon',
+  //             path: '/components/icon',
+  //           },
+  //         ],
+  //       },
 
-        {
-          group: 'Layout',
-          children: [
-            {
-              label: 'Divider',
-              path: '/components/divider',
-            },
-            {
-              label: 'Grid',
-              path: '/components/grid',
-            },
-          ],
-        },
-        {
-          group: 'Data Display',
-          children: [
-            {
-              label: 'Card',
-              path: '/components/card',
-            },
-          ],
-        },
-        {
-          group: 'Other',
-          children: Array.from(Array(20)).map((_, index) => {
-            return {
-              label: 'Mock Item ' + index,
-              path: '/components/mock/' + index,
-            }
-          }),
-        },
-      ]
-    }
-    if (loadState.routePath.startsWith('/docs/react')) {
-      return [
-        { label: 'Ant Design of React', path: '/docs/react/introduce' },
-        { label: 'Getting Started', path: '/docs/react/getting-started' },
-        ...Array.from(Array(20)).map((_, index) => {
-          return {
-            label: 'Mock Item ' + index,
-            path: '/docs/react/mock/' + index,
-          }
-        }),
-      ]
-    }
-    // don't render sidebar
-    return null
-  },
+  //       {
+  //         group: 'Layout',
+  //         children: [
+  //           {
+  //             label: 'Divider',
+  //             path: '/components/divider',
+  //           },
+  //           {
+  //             label: 'Grid',
+  //             path: '/components/grid',
+  //           },
+  //         ],
+  //       },
+  //       {
+  //         group: 'Data Display',
+  //         children: [
+  //           {
+  //             label: 'Card',
+  //             path: '/components/card',
+  //           },
+  //         ],
+  //       },
+  //       {
+  //         group: 'Other',
+  //         children: Array.from(Array(20)).map((_, index) => {
+  //           return {
+  //             label: 'Mock Item ' + index,
+  //             path: '/components/mock/' + index,
+  //           }
+  //         }),
+  //       },
+  //     ]
+  //   }
+  //   if (loadState.routePath.startsWith('/docs/react')) {
+  //     return [
+  //       { label: 'Ant Design of React', path: '/docs/react/introduce' },
+  //       { label: 'Getting Started', path: '/docs/react/getting-started' },
+  //       ...Array.from(Array(20)).map((_, index) => {
+  //         return {
+  //           label: 'Mock Item ' + index,
+  //           path: '/docs/react/mock/' + index,
+  //         }
+  //       }),
+  //     ]
+  //   }
+  //   // don't render sidebar
+  //   return null
+  // },
 })
