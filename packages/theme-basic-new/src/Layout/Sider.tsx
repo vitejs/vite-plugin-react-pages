68de807b0b0a8dee37d74d3ed730a49c34b22cf7
csr632 <632882184@qq.com>
2021-06-05 13:10:26 +0800

    improve defaultSideNav

--- packages/theme-basic-new/src/Layout/Sider.tsx
+++ packages/theme-basic-new/src/Layout/Sider.tsx
@@ -52,18 +52,31 @@ export function defaultSideNav({
   const seg = removeStartSlash(loadState.routePath).split('/')
 
   if (seg.length === 1) {
-    // show all first-level pages  ( like /xxx or / )
-    return Object.entries(staticData)
+    // this is a first-level page .e.g /xxx
+
+    /**
+     * current page is a index page for a page group
+     * .e.g /guide should not be treated like normal first-level page
+     * It should be grouped with /guide/start, /guide/advanced .etc
+     */
+    let currentPageIsGroupIndexPage = false
+    const result = Object.entries(staticData)
+      // show all first-level pages  ( like /xxx or / )
       .filter(([pagePath]) => {
         if (pagePath === '/404') return false
         if (removeStartSlash(pagePath).split('/').length === 1) {
-          const found = Object.keys(staticData).find((finding) =>
+          const foundSameGroupPage = Object.keys(staticData).find((finding) =>
             finding.startsWith(pagePath + '/')
           )
-          // this page belongs to another realm
-          if (found) return false
+          // pagePath page belongs to a page group
+          if (foundSameGroupPage) {
+            if (pagePath === loadState.routePath)
+              currentPageIsGroupIndexPage = true
+            return false
+          }
           return true
         }
+        return false
       })
       .map(([pagePath, pageStaticData]) => {
         return {
@@ -71,40 +84,62 @@ export function defaultSideNav({
           path: pagePath,
         }
       })
+    if (!currentPageIsGroupIndexPage) {
+      // this is a normal first-level page
+      // '/' page don't show sideNav if it is the only first-level page
+      if (result.length === 1 && loadState.routePath === '/') return null
+      return result
+    }
   }
 
-  // show all pages with pathPrefix in the side bar
+  // show all pages with this pathPrefix (.e.g /guide)
   const pathPrefix = '/' + seg[0]
 
-  const groups: Record<string, { pagePath: string; pageStaticData: any }[]> = {}
+  // sub-groups within this page group
+  const subGroups: Record<
+    string,
+    { pagePath: string; pageStaticData: any; pageName: string }[]
+  > = {}
   function ensureGroup(key: string) {
-    if (!groups[key]) return (groups[key] = [])
-    return groups[key]
+    if (!subGroups[key]) return (subGroups[key] = [])
+    return subGroups[key]
   }
 
   Object.entries(staticData).filter(([pagePath, pageStaticData]) => {
     if (pagePath === pathPrefix) {
-      ensureGroup('/').push({ pagePath, pageStaticData })
+      // this page is the '/' index page for this pathPrefix
+      // show it
+      ensureGroup('/').push({ pagePath, pageStaticData, pageName: '/' })
       return
     }
     if (pagePath.startsWith(`${pathPrefix}/`)) {
+      // this page is belongs to this pathPrefix
+      // show it
       const left = pagePath.substr(pathPrefix.length + 1)
       const leftSeg = left.split('/')
       if (leftSeg.length === 0) {
         throw new Error('leftSeg assertion fail')
       }
       if (leftSeg.length === 1) {
-        ensureGroup('/').push({ pagePath, pageStaticData })
+        // this page is first-level page(don't have sub-group) for this pathPrefix
+        ensureGroup('/').push({
+          pagePath,
+          pageStaticData,
+          pageName: leftSeg[0],
+        })
         return
       }
+      // this page belongs to a sub-group
+      // for example /pathPrefix/sub-group/pagename have sub-group named "sub-group"
       const groupKey = leftSeg[0]
-      ensureGroup(groupKey).push({ pagePath, pageStaticData })
+      const pageName = leftSeg.slice(1).join('/')
+      ensureGroup(groupKey).push({ pagePath, pageStaticData, pageName })
     }
   })
 
   const result: MenuConfig[] = []
 
-  Object.entries(groups)
+  Object.entries(subGroups)
     .sort(([groupKeyA], [groupKeyB]) => {
       if (groupKeyA === '/') return -1
       if (groupKeyB === '/') return 1
@@ -114,7 +149,7 @@ export function defaultSideNav({
       if (groupKey === '/') {
         pages.forEach((page) => {
           result.push({
-            label: page.pagePath,
+            label: page.pageName,
             path: page.pagePath,
           })
         })
@@ -124,14 +159,13 @@ export function defaultSideNav({
         group: groupKey,
         children: pages.map((page) => {
           return {
-            label: page.pagePath,
+            label: page.pageName,
             path: page.pagePath,
           }
         }),
       })
     })
-  console.log('groups', groups, result)
-
+  // console.log('pathPrefix', pathPrefix, 'subGroups', subGroups)
   return result
 }
 
