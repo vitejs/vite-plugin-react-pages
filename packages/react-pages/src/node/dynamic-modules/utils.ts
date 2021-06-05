72af48632337c6cf34159dcbfe5a7150ec50898d
csr632 <632882184@qq.com>
2021-06-06 02:19:53 +0800

    extract title from markdown as fallback

--- packages/react-pages/src/node/dynamic-modules/utils.ts
+++ packages/react-pages/src/node/dynamic-modules/utils.ts
@@ -5,16 +5,21 @@ import { File } from './PageStrategy'
 export async function extractStaticData(
   file: File
 ): Promise<{ sourceType: string; [key: string]: any }> {
+  const code = await file.read()
   switch (file.extname) {
     case 'md':
     case 'mdx':
-      const { data: frontmatter } = grayMatter(await file.read())
-      return { ...frontmatter, sourceType: 'md' }
+      const { data: frontmatter } = grayMatter(code)
+      const staticData: any = { ...frontmatter, sourceType: 'md' }
+      if (staticData.title === undefined) {
+        staticData.title = extractMarkdownTitle(code)
+      }
+      return staticData
     case 'js':
     case 'jsx':
     case 'ts':
     case 'tsx':
-      return { ...parse(extract(await file.read())), sourceType: 'js' }
+      return { ...parse(extract(code)), sourceType: 'js' }
     default:
       throw new Error(`unexpected extension name "${file.extname}"`)
   }
@@ -48,4 +53,9 @@ export class PendingList {
       })
     })
   }
+}
+
+function extractMarkdownTitle(code: string) {
+  const match = code.match(/^# (.*)$/m)
+  return match?.[1]
 }
