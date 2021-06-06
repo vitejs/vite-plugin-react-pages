77f62ea6c166ef4b102fc9e1779aa0cf0fc912de
csr632 <632882184@qq.com>
2021-06-07 00:23:31 +0800

    improve fixture

--- packages/playground/use-theme-new/pages/components/overview/index$.tsx
+++ packages/playground/use-theme-new/pages/components/overview/index$.tsx
@@ -1,12 +1,17 @@
+/**
+ * @title Overview
+ * @order 0
+ */
+
 import React from 'react'
 
 interface Props {}
 
 const Component: React.FC<Props> = (props) => {
   return (
-    <div style={{ border: '1px solid red', height: 2400 }}>
-      Components Overview
-      <p>Long React content...</p>
+    <div style={{ height: 2400, margin: '0px 12px 12px' }}>
+      <h1>Components Overview</h1>
+      <p>This is complex page built with React...</p>
     </div>
   )
 }
