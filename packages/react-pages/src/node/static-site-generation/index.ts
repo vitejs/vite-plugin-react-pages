780c7755c24f9ef788519277f0c937f4ce44476e
csr632 <632882184@qq.com>
2021-06-13 20:13:16 +0800

    add TODO comment

--- packages/react-pages/src/node/static-site-generation/index.ts
+++ packages/react-pages/src/node/static-site-generation/index.ts
@@ -46,6 +46,7 @@ export async function ssrBuild(
     ssr: {
       external: ['react', 'react-router-dom', 'react-dom', 'react-dom/server'],
       noExternal: [
+        // TODO: remove this
         'vite-pages-theme-basic',
         'vite-pages-theme-doc',
         'vite-plugin-react-pages',
