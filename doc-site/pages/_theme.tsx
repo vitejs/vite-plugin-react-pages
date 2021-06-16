d5f44b3c6ccae5996ac4c26085b1ad78f61cbc12
csr632 <632882184@qq.com>
2021-06-17 02:31:41 +0800

    doc-site: add subGroup title

--- doc-site/pages/_theme.tsx
+++ doc-site/pages/_theme.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { createTheme } from 'vite-pages-theme-doc'
+import { createTheme, defaultSideNavs } from 'vite-pages-theme-doc'
 
 export default createTheme({
   topNavs: [
@@ -10,4 +10,20 @@ export default createTheme({
     },
   ],
   logo: <div style={{ marginLeft: 40, fontSize: '20px' }}>Vite Pages</div>,
+  sideNavs(ctx) {
+    return defaultSideNavs(ctx, {
+      groupConfig: {
+        '/': {
+          advanced: {
+            label: 'Advanced',
+            order: 2,
+          },
+          'upgrade-guides': {
+            label: 'Upgrade Guides',
+            order: 3,
+          },
+        },
+      },
+    })
+  },
 })
