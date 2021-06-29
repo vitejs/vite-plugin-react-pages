bfb40347cb4d640e132611b0256730a6fa61b118
csr632 <632882184@qq.com>
2021-06-29 10:06:15 +0800

    save work

--- packages/react-pages/src/node/dynamic-modules/VirtualModules.ts
+++ packages/react-pages/src/node/dynamic-modules/VirtualModules.ts
@@ -4,13 +4,10 @@ import chokidar, { FSWatcher } from 'chokidar'
 import slash from 'slash'
 
 export class VirtualModuleGraph {
-  private readonly modules: Modules = {}
-
-  private updateQueue = new UpdateQueue()
-
-  getModuleContent(moduleId: string): string | null {
-    return null
-  }
+  /**
+   * the module inside this graph may be virtule module or real fs module
+   */
+  private readonly modules: Map<string, Module> = new Map()
 
   /**
    * Serialize the update works (instead of do them concurrently)
@@ -23,27 +20,17 @@ export class VirtualModuleGraph {
    * because they don't need to manually clean the previous effects of a file
    * when a file updates.
    */
+  private updateQueue = new UpdateQueue()
+
+  getModuleData(moduleId: string): any[] {
+    const module = this.modules.get(moduleId)
+    if (!module) return []
+    return module.getData()
+  }
+
   scheduleUpdate(updaterId: string, updater: Update['updater']) {
     this.updateQueue.push(updaterId, updater)
   }
-
-  // private updateModule(moduleId: string, data: any, upstreamModuleId: string) {
-  //   const m = this.ensureModule(moduleId)
-  //   m.data[upstreamModuleId] = data
-
-  //   const upstream = this.ensureModule(upstreamModuleId)
-  //   upstream.downstream.add(moduleId)
-  // }
-
-  // private ensureModule(moduleId: string) {
-  //   if (this.modules[moduleId]) return this.modules[moduleId]
-  //   return (this.modules[moduleId] = {
-  //     id: moduleId,
-  //     // dirty: false,
-  //     data: {},
-  //     downstream: new Set(),
-  //   })
-  // }
 
   watchModules() {}
 
@@ -51,18 +38,51 @@ export class VirtualModuleGraph {
 
   onModuleUpdate() {}
 
-  private createUpdateAPIs(): UpdaterAPIs {
+  private ensureModule(
+    moduleId: string,
+    newModuleType: Module['moduleType']
+  ): Module {
+    let result = this.modules.get(moduleId)
+    if (!result) {
+      result = new Module(moduleId, newModuleType)
+      this.modules.set(moduleId, result)
+    }
+    return result
+  }
+
+  private createUpdateAPIs(updaterId: string): UpdaterAPIs {
+    const _this = this
     return {
-      updateModule(moduleId: string, data: any, upstreamModuleId: string) {},
-      getModuleData(moduleId: string, data: any, upstreamModuleId: string) {},
+      updateModule(moduleId: string, data: any, upstreamModuleId: string) {
+        // fromModule must be a fs module if it doesn't exist before
+        const fromModule = _this.ensureModule(upstreamModuleId, 'fs')
+        // target module must be a virtule module
+        const toModule = _this.ensureModule(moduleId, 'virtule')
+        Edge.addEdge(fromModule, toModule, data, updaterId)
+      },
+      getModuleData(moduleId: string) {
+        return _this.getModuleData(moduleId)
+      },
+      deleteFsModule(moduleId: string) {
+        const module = _this.modules.get(moduleId)
+        if (!module) return
+        
+      },
     }
   }
 
-  async executeUpdates() {
-    const update = this.updateQueue.pop()
-    if (!update) return
-    const apis = this.createUpdateAPIs()
-    await update.updater(apis)
+  private cleanUpdateEffects(updaterId: string) {
+    deleteAllEdgesWithUpdaterId(updaterId)
+  }
+  public async executeUpdates() {
+    while (true) {
+      const update = this.updateQueue.pop()
+      if (!update) return
+
+      this.cleanUpdateEffects(update.updaterId)
+      const apis = this.createUpdateAPIs(update.updaterId)
+      await update.updater(apis)
+    }
   }
 }
 
@@ -97,39 +117,35 @@ class UpdateQueue {
   }
 }
 
-// the module inside this graph may be virtule module or real fs module
-interface Modules {
-  [moduleId: string]: Module
-}
-
-const VIRTULE_MODULE_PREFIX = '/@vp_virtule'
-
+/**
+ * Module may be fs module or virtule module
+ * - fs modules must not have data. But they can have downstreams.
+ *    In other words, fs modules can only be data source.
+ * - virtule modules can have both data and downstreams.
+ *    If a virtule module doesn't have any incoming edge (.i.e data), it doesn't exists.
+ */
 class Module {
-  constructor(public id: string) {}
+  constructor(public id: string, public moduleType: 'fs' | 'virtule') {}
 
   /** has updated, need to update it's downstream */
   // dirty: boolean
 
   /**
-   * the data sources of this virtule module
-   * they are incoming edges in the graph
+   * incoming edges of the node
+   * indicating the data of this virtule module
    *
    * real fs module won't need this
    */
   private data: Set<Edge> = new Set()
 
   /**
-   * which modules depend on this module
-   * they are outcoming edges in the graph
+   * outcoming edges of the node
+   * indicating which modules depend on this module
    *
    * it is needed because we need to update downstream modules
    * when a fs module is deleted
    */
   private downstream: Set<Edge> = new Set()
-
-  public get isVirtuleModule() {
-    return this.id.startsWith(VIRTULE_MODULE_PREFIX)
-  }
 
   /** bind incoming edge */
   public addData(edge: Edge) {
@@ -137,6 +153,9 @@ class Module {
   }
   public removeData(edge: Edge) {
     this.data.delete(edge)
+  }
+  public getData() {
+    return Array.from(this.data).map(({ data }) => data)
   }
 
   /** bind outcoming edge */
@@ -149,7 +168,7 @@ class Module {
 }
 
 const mapUpdaterIdToEdges = new Map<string, Set<Edge>>()
-function linkEdgeWithUpdaterId(edge: Edge) {
+function bindEdgeWithUpdaterId(edge: Edge) {
   const { updaterId } = edge
   let edges = mapUpdaterIdToEdges.get(updaterId)
   if (!edges) {
@@ -158,19 +177,30 @@ function linkEdgeWithUpdaterId(edge: Edge) {
   }
   edges.add(edge)
 }
-function unlinkEdgeWithUpdaterId(edge: Edge) {
+function unbindEdgeWithUpdaterId(edge: Edge) {
   const { updaterId } = edge
-  let edges = mapUpdaterIdToEdges.get(updaterId)
-  if (!edges) throw new Error(`assertion fail: unlinkEdgeWithUpdaterId`)
+  const edges = mapUpdaterIdToEdges.get(updaterId)
+  if (!edges || !edges.has(edge))
+    throw new Error(`assertion fail: unlinkEdgeWithUpdaterId`)
   edges.delete(edge)
+}
+function deleteAllEdgesWithUpdaterId(updaterId: string) {
+  const edges = mapUpdaterIdToEdges.get(updaterId)
+  if (!edges) return
+  edges.forEach((edge) => {
+    edge.unlink()
+  })
+  edges.clear()
 }
 
 class Edge {
   static addEdge(from: Module, to: Module, data: any, updaterId: string) {
     const edge = new Edge(from, to, data, updaterId)
+    if (to.moduleType === 'fs')
+      throw new Error(`target module must be a virtule module`)
     from.addDownStream(edge)
     to.addData(edge)
-    linkEdgeWithUpdaterId(edge)
+    bindEdgeWithUpdaterId(edge)
   }
 
   private constructor(
@@ -185,12 +215,13 @@ class Edge {
     if (this.hasUnlinked) return
     this.from.removeDownStream(this)
     this.to.removeData(this)
-    unlinkEdgeWithUpdaterId(this)
+    unbindEdgeWithUpdaterId(this)
     this.hasUnlinked = true
   }
 }
 
 interface UpdaterAPIs {
   updateModule(moduleId: string, data: any, upstreamModuleId: string): void
-  getModuleData(moduleId: string, data: any, upstreamModuleId: string): void
+  getModuleData(moduleId: string): any[]
+  deleteFsModule(moduleId: string): void
 }
