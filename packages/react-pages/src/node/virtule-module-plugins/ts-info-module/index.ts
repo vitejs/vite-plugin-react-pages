e8d676a836ff1832984ed895e6b8d238110fd4f8
csr632 <632882184@qq.com>
2021-09-12 00:23:46 +0800

    add TODO

--- packages/react-pages/src/node/virtule-module-plugins/ts-info-module/index.ts
+++ packages/react-pages/src/node/virtule-module-plugins/ts-info-module/index.ts
@@ -3,6 +3,20 @@ import { collectInterfaceInfo } from './extract'
 
 const PROXY_PREFIX = '/@react-pages/tsInfo'
 
+/**
+ * TODO:
+ * currently we only watch one sourceFilePath for each virtule module(proxyModule).
+ * but if the sourceFilePath import other modules, and when that module has updated,
+ * TsInfoModuleManager won't notice that.
+ * we need a way to create virtule modules with dependencies.
+ *
+ * currently it is ok to tell users to write a whole type doc in a single file.
+ * so supporting virtule modules with dependencies is not needed.
+ * but supporting that feature would make our virtule module system more powerful.
+ *
+ * the same goes for DemoModuleManager.
+ */
+
 export class TsInfoModuleManager {
   private pmm = new ProxyModulesManager(PROXY_PREFIX)
 
