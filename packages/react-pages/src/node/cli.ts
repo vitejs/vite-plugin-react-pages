6e8e14c84389343e993ae4d1724636fd53c3fdc2
csr632 <632882184@qq.com>
2022-01-11 23:29:44 +0800

    clearify vite-pages cli usage

--- packages/react-pages/src/node/cli.ts
+++ packages/react-pages/src/node/cli.ts
@@ -8,15 +8,15 @@ const argv: any = minimist(process.argv.slice(2))
 console.log(chalk.cyan(`vite-pages v${require('../../package.json').version}`))
 console.log(chalk.cyan(`vite v${require('vite/package.json').version}`))
 
-const command = argv._[0]
-const root = argv._[command ? 1 : 0]
+// cli usage: vite-pages ssr [root] [vite config like --outDir or --configFile]
+const [command, root] = argv._
 if (root) {
   argv.root = root
 }
 
 ;(async () => {
   if (!command || command === 'ssr') {
-    // user can pass --root or --configFile
+    // user can pass in vite config like --outDir or --configFile
     const viteConfig = await resolveConfig(argv, 'build')
     const thisPlugin = viteConfig.plugins.find((plugin) => {
       return plugin.name === 'vite-plugin-react-pages'
@@ -29,6 +29,8 @@ if (root) {
       process.exit(1)
     })
   } else {
-    console.error(`invalid command. command should be "ssr"`)
+    console.error(
+      `[vite-pages] Invalid command. CLI usage: vite-pages ssr [root] [vite config like --outDir or --configFile]`
+    )
   }
 })()
