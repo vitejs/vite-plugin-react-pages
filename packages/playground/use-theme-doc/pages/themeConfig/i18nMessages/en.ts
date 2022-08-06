c5ed3753c5e657e76078643015aa1850db4538e4
csr632 <632882184@qq.com>
2022-08-06 15:47:29 +0800

    improve playground i18n

--- packages/playground/use-theme-doc/pages/themeConfig/i18nMessages/en.ts
+++ packages/playground/use-theme-doc/pages/themeConfig/i18nMessages/en.ts
@@ -1,3 +1,5 @@
 export const messages = {
-  'page1.content': 'This is page1. This is a page defined with React component.'
+  'page.page1.content':
+    'This is page1. This is a page defined with React component.',
+  'page.resources.content': 'Resources Page',
 }
--- packages/playground/use-theme-doc/pages/themeConfig/i18nMessages/zh.ts
+++ packages/playground/use-theme-doc/pages/themeConfig/i18nMessages/zh.ts
@@ -1,3 +1,5 @@
-export const messages = {
-  'page1.content': '这是页面1。这是一个通过React组件来编写的页面。',
+import type { MessagesType } from '.'
+export const messages: MessagesType = {
+  'page.page1.content': '这是页面1。这是一个通过React组件来编写的页面。',
+  'page.resources.content': '资源页面',
 }
