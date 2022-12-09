dbdf2f6c4df1b07b0c596b2b31598c02383a4b06
csr632 <632882184@qq.com>
2022-12-09 22:15:15 +0800

    jotai provider-less mode

--- packages/react-pages/src/client/entries/csr.tsx
+++ packages/react-pages/src/client/entries/csr.tsx
@@ -6,17 +6,13 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 
-import { Provider as Jotai } from 'jotai'
 import ClientAppWrapper from './ClientAppWrapper'
 import App from '../App'
 
-let app = <App />
-if (import.meta.hot) {
-  // We only need Jotai to manage hmr state update during dev
-  // @ts-ignore
-  app = <Jotai>{app}</Jotai>
-}
-
 const container = document.getElementById('root')!
 const root = createRoot(container)
-root.render(<ClientAppWrapper>{app}</ClientAppWrapper>)
+root.render(
+  <ClientAppWrapper>
+    <App />
+  </ClientAppWrapper>
+)
