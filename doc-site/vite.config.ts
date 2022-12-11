d35f61ec52e4e6856d4925f62b59ef0e6906ec7e
csr632 <632882184@qq.com>
2022-12-11 22:08:48 +0800

    improve vite setup

--- doc-site/vite.config.ts
+++ doc-site/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import pages from 'vite-plugin-react-pages'
 
 export default defineConfig({
-  plugins: [react(), ...(await pages({}))],
+  plugins: [react(), pages()],
   base:
     process.env.GITHUB_PAGES_DEPLOY === 'true'
       ? '/vite-plugin-react-pages/'
--- packages/playground/use-theme-doc/vite.config.ts
+++ packages/playground/use-theme-doc/vite.config.ts
@@ -3,25 +3,23 @@ import * as path from 'path'
 import react from '@vitejs/plugin-react'
 import pages from 'vite-plugin-react-pages'
 
-export default defineConfig(async () => {
-  return {
-    resolve: {
-      alias: {
-        '~pages/': `${path.join(__dirname, 'pages')}/`,
-      },
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~pages/': `${path.join(__dirname, 'pages')}/`,
     },
-    plugins: [
-      react(),
-      ...(await pages({
-        pagesDir: path.join(__dirname, 'pages'),
-        // useHashRouter: true
-      })),
-    ],
-    // theme local dev
-    css: {
-      modules: {
-        generateScopedName: `vp-local-[local]`,
-      },
+  },
+  plugins: [
+    react(),
+    pages({
+      pagesDir: path.join(__dirname, 'pages'),
+      // useHashRouter: true
+    }),
+  ],
+  // theme local dev
+  css: {
+    modules: {
+      generateScopedName: `vp-local-[local]`,
     },
-  }
+  },
 })
