518e3e94734d7feef7f47b4e44c70596b35b9b22
csr632 <632882184@qq.com>
2022-12-11 21:23:54 +0800

    update setup

--- doc-site/vite.config.ts
+++ doc-site/vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { setupPlugins } from 'vite-plugin-react-pages'
+import pages from 'vite-plugin-react-pages'
 
 export default defineConfig({
-  plugins: [react(), ...(await setupPlugins({}))],
+  plugins: [react(), ...(await pages({}))],
   base:
     process.env.GITHUB_PAGES_DEPLOY === 'true'
       ? '/vite-plugin-react-pages/'
--- packages/playground/use-theme-doc/vite.config.ts
+++ packages/playground/use-theme-doc/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import * as path from 'path'
 import react from '@vitejs/plugin-react'
-import { setupPlugins } from 'vite-plugin-react-pages'
+import pages from 'vite-plugin-react-pages'
 
 export default defineConfig(async () => {
   return {
@@ -12,21 +12,13 @@ export default defineConfig(async () => {
     },
     plugins: [
       react(),
-      ...(await setupPlugins({
+      ...(await pages({
         pagesDir: path.join(__dirname, 'pages'),
         // useHashRouter: true
       })),
     ],
     // theme local dev
     css: {
-      preprocessorOptions: {
-        less: {
-          modifyVars: {
-            'ant-prefix': 'vp-antd',
-          },
-          javascriptEnabled: true,
-        },
-      },
       modules: {
         generateScopedName: `vp-local-[local]`,
       },
