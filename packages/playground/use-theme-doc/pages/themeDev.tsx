7cba64a87b55d7f4880bcc846e4326e87fb473f3
csr632 <632882184@qq.com>
2022-12-17 23:57:21 +0800

    cleanup

--- packages/playground/use-theme-doc/pages/themeDev.tsx
+++ packages/playground/use-theme-doc/pages/themeDev.tsx
@@ -1,4 +1,5 @@
 // import the src directly instead of build output
 // useful for local development in this repo
 // actual users don't need to do this, they should import from 'vite-pages-theme-doc'
+// export * from 'vite-pages-theme-doc/src/index'
 export * from 'vite-pages-theme-doc'
--- packages/playground/use-theme-doc/vite.config.ts
+++ packages/playground/use-theme-doc/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import * as path from 'path'
 import react from '@vitejs/plugin-react'
 import pages from 'vite-plugin-react-pages'
-import theme from 'vite-pages-theme-doc/vitePlugin'
 
 export default defineConfig({
   resolve: {
@@ -16,7 +15,6 @@ export default defineConfig({
       pagesDir: path.join(__dirname, 'pages'),
       // useHashRouter: true
     }),
-    theme()
   ],
   // theme local dev
   css: {
