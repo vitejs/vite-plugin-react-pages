2b2c33aca69e76f89a52e16f27840106c8e56fdb
csr632 <632882184@qq.com>
2023-04-11 00:32:16 +0800

    improve comment for dev

--- packages/playground/use-theme-doc/pages/themeDev.tsx
+++ packages/playground/use-theme-doc/pages/themeDev.tsx
@@ -1,5 +1,6 @@
 // import the src directly instead of build output
-// useful for local development in this repo
-// actual users don't need to do this, they should import from 'vite-pages-theme-doc'
+// useful for local development in this repo (and only used for dev)
+// this should be used with viteConfig.css.modules.generateScopedName setup: https://github.com/vitejs/vite-plugin-react-pages/blob/a295d2902ef2d69c7808db2be8d182807ead5a3e/packages/playground/use-theme-doc/vite.config.ts#L20
 export * from 'vite-pages-theme-doc/src/index'
+// actual users shouldn't do this, they should import directly from 'vite-pages-theme-doc':
 // export * from 'vite-pages-theme-doc'
