13251af5d4e3f0fb8283b5a4a45ed06f42bc047d
csr632 <632882184@qq.com>
2023-07-23 22:30:04 +0800

    update templates to tell users that they can also use @vitejs/plugin-react-swc

--- packages/create-project/template-app/vite.config.ts
+++ packages/create-project/template-app/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import * as path from 'path'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // you can also use @vitejs/plugin-react-swc
 import pages from 'vite-plugin-react-pages'
 
 export default defineConfig({
